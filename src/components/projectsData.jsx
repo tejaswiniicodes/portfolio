79bad1d66708154c1306f3d3828bf5ddea433832
tejaswini <tejaswinikadu85@gmail.com>
2026-08-12 21:46:50 +0530

    Commit

--- src/components/projectsData.jsx
+++ src/components/projectsData.jsx
@@ -1,28 +1,33 @@
+import mojitoImage from '../assets/projectSS/Mojito.png'
+import skyMartImage from '../assets/projectSS/SkyMart.png'
+import studySprintImage from '../assets/projectSS/StudySprint.png'
+import finTrackImage from '../assets/projectSS/FinTrack.png'
+
 const projects = [
   {
     id: 1,
-    name: "Mojito",
-    image: "https://github.com/tejaswiniicodes/portfolio/blob/1f38796af8d2451d89e889f2478c216531102385/src/assets/projectSS/Mojito.png",
-    link: "https://cocktaildev.vercel.app/"
+    name: 'Mojito',
+    image: mojitoImage,
+    link: 'https://cocktaildev.vercel.app/',
   },
   {
     id: 2,
-    name: "SkyMart",
-    image: "src/assets/projectSS/SkyMart.png",
-    link: "https://tejaswinikadu85-debug.github.io/SkyMart/"
+    name: 'SkyMart',
+    image: skyMartImage,
+    link: 'https://tejaswinikadu85-debug.github.io/SkyMart/',
   },
   {
     id: 3,
-    name: "StudySprint",
-    image: "src/assets/projectSS/StudySprint.png",
-    link: "https://tejaswinikadu85-debug.github.io/StudySprint/"
+    name: 'StudySprint',
+    image: studySprintImage,
+    link: 'https://tejaswinikadu85-debug.github.io/StudySprint/',
   },
   {
     id: 4,
-    name: "FinTrack",
-    image: "src/assets/projectSS/FinTrack.png",
-    link: "https://fin-track-pro-personal-finance-trac-rho.vercel.app/"
-  }
-];
+    name: 'FinTrack',
+    image: finTrackImage,
+    link: 'https://fin-track-pro-personal-finance-trac-rho.vercel.app/',
+  },
+]
 
-export default projects;
+export default projects
